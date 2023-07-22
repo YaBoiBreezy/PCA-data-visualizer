51294f5efa05308a67fde7402cbce105342e9081
Alex Breeze <91281967+YaBoiBreezy@users.noreply.github.com>
2023-07-22 16:55:32 -0400

    Made it better by adding auto point generation in gaussian clusters

--- main.py
+++ main.py
@@ -4,8 +4,30 @@
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 
-def get_file(filename):
+def makePoint(numDim):
+ p=[]
+ for _ in range(numDim):
+  p.append(random.uniform(0,1))
+ return p
+
+def makePointGauss(center,numDim):
+ p=[]
+ for d in range(numDim):
+  p.append(random.gauss(center[d],0.1))
+ return p
+
+def makeFile(numCenters, numPoints, numDim):
+ matrix=[]
+ for _ in range(numCenters):
+  center=makePoint(numDim)
+  for _ in range(numPoints):
+   matrix.append(makePointGauss(center,numDim))
+ matrix=np.array(matrix)
+ return matrix
+
+def getFile(filename):
  matrix=[]
  f=open(filename, 'r') #if file not found, then fails here
  lines = f.readlines()
@@ -15,9 +37,10 @@ def get_file(filename):
  return matrix
 
 def doPCA(matrix):
- pca=PCA(n_components=5)
- pca.fit(matrix)
- matrix=pca.transform(matrix) #do PCA on matrix
+ if matrix.shape[1]>5: #do PCA on matrix
+  pca=PCA(n_components=5)
+  pca.fit(matrix)
+  matrix=pca.transform(matrix)
  matrix=matrix-matrix.min(axis=0) #shift each column so minCol=0
  matrix=matrix / matrix.max(axis=0) #reduce each colMax to 1
  return matrix
@@ -29,14 +52,16 @@ def display(matrix):
 
 def main():
  print("Welcome to the 5-dimensional scatterplot demo!")
- print("Please make a file with data points, following these rules:")
- print("Each line is one point, consisting of a comma separated list of coordinates")
- print("5 dimensions minimum per point, and each point must have the same number of dimensions")
- print("Then enter the path to the file and press enter, or use the builtin demo \"./test1.txt\"")
- #filename=input()
- filename="./test1.txt"
- print("Opening file")
- matrix=get_file(filename)
+ print("Please enter 3 space-separated ints for numCenters, numPoints (per center), and numDims, e.g. 4 10 5")
+ print("Values must be >=1, >=1, >=5, respectively")
+ print("OR")
+ print("Make a file with data points similar to test1.txt and enter the path to it. Minimum 5 coordinates per point (aka per line)")
+ inp=input()
+ if ' ' in inp:
+  inp=inp.split(' ')
+  matrix=makeFile(int(inp[0]), int(inp[1]), int(inp[2]))
+ else:
+  matrix=getFile(inp)
  print(matrix)
  matrix=doPCA(matrix)
  print(matrix)
